ed96fb8ba76d1107c5b58c8ea244aa197ee63522
Melina <melina.rivera@gmail.com>
2026-04-15 10:48:35 +0200

    cambios en gitignore

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import db from "mongoose";
-import express from "express";
+import db from 'mongoose';
+import express from 'express';
 
 const app = express();
 
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("hola mundo");
+app.get('/', (req, res)=> {
+
+    res.send("hola mundo")
 });
 
-app.listen(port, ()=>[
-    console.log ('el servidor arranco en la url http://localhost${port}')
-    
-])
+app.listen(port, ()=>{
+    console.log(`el servidor aranco en la url http://localhost:${port}`)
+})
