b4e3442257fc4c7b578128a1066ca2e2206989e3
Melina <melina.rivera@gmail.com>
2026-04-15 17:38:54 +0200

    cambios en index y pokeapi

--- pokeapi.js
+++ pokeapi.js
@@ -2,17 +2,11 @@
 export async function obtenerSpritePokemon(numero) {
   const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${numero}`);
 
-  // Si la respuesta no fue correcta, lanzamos un error
+  // si no es correcto pone error
   if (!respuesta.ok) {
     throw new Error("No se pudo obtener el pokemon");
   }
 
-  const data = await respuesta.json();
+  const data = await respuesta.json(); 
 
-  // Devolvemos un objeto más limpio, solo con lo que necesitamos
-  return {
-    numero: numero,
-    nombre: data.name,
-    sprite: data.sprites.front_default
-  };
 }
